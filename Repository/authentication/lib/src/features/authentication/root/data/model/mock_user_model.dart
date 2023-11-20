82d4ca554afee2bd44b4a4c20b3a8ee78dd12a6a
Mozammal Hossain <144788896+Mozammal-hossain@users.noreply.github.com>
2023-11-20 14:38:44 +0600

    fix: Fixed offline image issue

--- Repository/authentication/lib/src/features/authentication/root/data/model/mock_user_model.dart
+++ Repository/authentication/lib/src/features/authentication/root/data/model/mock_user_model.dart
@@ -3,7 +3,6 @@ class MockUserModel {
   String email;
   String firstName;
   String lastName;
-  String fullName;
   String role;
   String password;
   String otp;
@@ -13,9 +12,10 @@ class MockUserModel {
     this.email = 'foo@foo.com',
     this.firstName = 'Mr. Foo',
     this.lastName = 'Khan',
-    this.fullName = 'Mr. Foo Khan',
     this.role = 'User',
     this.password = 'Foo@1234',
     this.otp = '123456',
   });
+
+  get fullName => '$firstName $lastName';
 }
