e6a99ef9513d5fa866d03cc12fc5e6a959ca6cda
Muhit Mahmud (BS23) <162097645+muhitmahmudbs23@users.noreply.github.com>
2024-04-30 14:48:30 +0600

    refactor: Added text theme support

--- Repository/authentication/lib/src/core/theme/theme.dart
+++ Repository/authentication/lib/src/core/theme/theme.dart
@@ -1,6 +1,8 @@
 import 'package:auth_module/src/core/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'typography/fonts.dart';
 import 'typography/style.dart';
 
 export 'colors.dart';
@@ -11,33 +13,49 @@ part 'app_bar_theme.dart';
 
 part 'outlined_button_theme.dart';
 
-
 part 'input_decoration_theme.dart';
+part 'typography/text_theme.dart';
 
 class GlobalThemeData {
   static final ThemeData _lightTheme = ThemeData(
-    useMaterial3: true,
-    colorScheme: lightColorScheme,
-    textSelectionTheme:
-         TextSelectionThemeData(cursorColor: lightColorScheme.primary),
-    inputDecorationTheme: CustomInputDecorationTheme.light(),
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: CustomOutlinedButtonStyle.light(),
-    ),
-    appBarTheme: CustomAppBarTheme.light(),
-  );
+      useMaterial3: true,
+      colorScheme: lightColorScheme,
+      textSelectionTheme:
+          TextSelectionThemeData(cursorColor: lightColorScheme.primary),
+      inputDecorationTheme: CustomInputDecorationTheme.light(),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: CustomOutlinedButtonStyle.light(),
+      ),
+      appBarTheme: CustomAppBarTheme.light(),
+      fontFamily: FontConstants.fontFamilyCaros,
+      focusColor: lightColorScheme.primary,
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: lightColorScheme.background,
+      textTheme: _textTheme,
+      dividerTheme: DividerThemeData(
+        color: lightColorScheme.surfaceVariant,
+        thickness: 1,
+      ));
 
   static final ThemeData _darkTheme = ThemeData(
-    useMaterial3: true,
-    colorScheme: darkColorScheme,
-    appBarTheme: CustomAppBarTheme.dark(),
-    textSelectionTheme:
-         TextSelectionThemeData(cursorColor: darkColorScheme.primary),
-    inputDecorationTheme: CustomInputDecorationTheme.dark(),
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: CustomOutlinedButtonStyle.dark(),
-    ),
-  );
+      useMaterial3: true,
+      colorScheme: darkColorScheme,
+      appBarTheme: CustomAppBarTheme.dark(),
+      textSelectionTheme:
+          TextSelectionThemeData(cursorColor: darkColorScheme.primary),
+      inputDecorationTheme: CustomInputDecorationTheme.dark(),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: CustomOutlinedButtonStyle.dark(),
+      ),
+      fontFamily: FontConstants.fontFamilyCaros,
+      focusColor: darkColorScheme.primary,
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: darkColorScheme.background,
+      textTheme: _textTheme,
+      dividerTheme: DividerThemeData(
+        color: darkColorScheme.onSurfaceVariant,
+        thickness: 1,
+      ));
 
   static ThemeData get lightTheme => _lightTheme;
 
---
+++ Repository/authentication/lib/src/core/theme/typography/text_theme.dart
@@ -0,0 +1,13 @@
+part of '../theme.dart';
+
+const TextTheme _textTheme = TextTheme(
+  titleSmall: _titleSmall,
+  titleMedium: _titleMedium,
+);
+
+const TextStyle _titleSmall = TextStyle();
+
+const TextStyle _titleMedium = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
